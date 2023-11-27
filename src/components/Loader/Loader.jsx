522f109ef7dacd79d1b8a69ea2dbb0ff0b3bc881
hodakivskaK <96114298+hodakivskaK@users.noreply.github.com>
2023-11-27 13:05:20 +0000

    Deploying to gh-pages from @ hodakivskaK/Dashboard-Questify@86d41d99f6e5f7936af6b2010a81298646f894f4 🚀

--- src/components/Loader/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import Spinner from 'react-bootstrap/Spinner'
-import s from './Loader.module.css'
-
-export default function Loader() {
-    return (
-        <Spinner animation="grow" variant="dark" />
-      )
-}
