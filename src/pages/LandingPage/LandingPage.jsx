2bde852bec0b3a18cd6d7aa9e39a533600760631
Chris <96114298+hodakivskaK@users.noreply.github.com>
2023-12-13 21:40:16 +0200

    fix deploy

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import s from './LandingPage.module.css'
 
 import { IoIosArrowRoundForward } from "react-icons/io";
-import { RxTarget } from "react-icons/rx";
 
 
 export default function LandingPage(){
